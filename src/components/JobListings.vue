d548dfdbe0a03d9d51e41870e39693a5c26acf81
L0GI0 <logiocorp@gmail.com>
2025-06-28 12:15:05 +0200

    feat: add pulse loader fo jobs

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -2,6 +2,7 @@
 import jobData from '@/jobs.json'
 import JobListing from './JobListing.vue'
 import { RouterLink } from 'vue-router';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 
 import { reactive, defineProps, onMounted } from 'vue';
 
@@ -38,7 +39,10 @@ onMounted(async () => {
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
                 Browse Jobs
             </h2>
-            <div class="grid grid-cols md:grid-cols-3 gap-6 ">
+            <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+                <PulseLoader/>
+            </div>
+            <div v-if="!isLoading" class="grid grid-cols md:grid-cols-3 gap-6 ">
                 <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job"/>
             </div>
         </div>
