7d403906fa9d2c805506a20b8bf9892f43bbd414
L0GI0 <logiocorp@gmail.com>
2025-06-28 20:06:28 +0200

    feat: connect to live server

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://vue-jobs-server-v22s.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
